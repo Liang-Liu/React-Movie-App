0740d64c1eb390612e2da9ac48b6777f416ac2cd
Liang-Liu <82113781+Liang-Liu@users.noreply.github.com>
2021-08-10 15:00:27 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useEffect } from "react";
+import Movie from "./components/Movie.js";
+import Form from "./components/Form.js";
+
+const apikey = "7f892db17123b0f41de97b678f673660";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [moviesArr, setMoviesArr] = useState([]);
+
+	useEffect(() => {
+		async function fetchPopularMovies() {
+			const res = await fetch(
+				`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}`
+			);
+			const resJson = await res.json();
+			setMoviesArr(resJson.results);
+		}
+		fetchPopularMovies();
+	}, []);
+
+	function searchMovieArr(MoviesArr) {
+		setMoviesArr(MoviesArr);
+	}
+
+	return (
+		<div className="App">
+			<header>
+				<Form searchMovieArrFunc={searchMovieArr} apikey={apikey} />
+			</header>
+			<main>
+				{moviesArr.map((ele, idx) => (
+					<Movie movie={ele} key={idx} />
+				))}
+			</main>
+		</div>
+	);
 }
 
 export default App;
